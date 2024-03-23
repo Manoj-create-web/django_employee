8d5eaa0161b5bb73693a667d585a433e6e8051ec
Manoj-creates-web <manojm7876121@gmail.com>
2024-03-23 13:35:05 +0530

    Initial commit

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ python manage.py makemigrations
 # Apply migrations
 python manage.py migrate
 
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('manoj', 'manoj@abc.com', 'superuser')" | python manage.py shell
+
 # Collect static files
 python manage.py collectstatic --noinput
